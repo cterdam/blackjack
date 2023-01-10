6f9a2064136b0134267a231782a23201a9ef5c5a
Josh Lee <llz.joshlee@gmail.com>
2023-01-09 22:39:26 -0500

    add constants check

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,13 +1,24 @@
 from blackjack.game import Game
+from blackjack.card import Card
 from blackjack.game_config import GameConfig
 
-"""
-TODO
 
-- cut_card only appears once in the playing deck
-- initial_hand_size (int) >= 0
-- rank2value sends each of 2-K to an int
-"""
+def test_constants():
+
+    # Cut card should only appear once in the play deck
+    g = Game(GameConfig())
+    assert g.deck.deck.count(Game.cut_card) == 1
+
+    # Blackjack value should be int or float
+    assert type(Game.blackjack_value) in (int, float)
+
+    # Initial hand size should be nonnegative int
+    assert type(Game.initial_hand_size) is int
+    assert Game.initial_hand_size >= 0
+
+    # rank2value should send each of 2-K to int or float
+    for r in (Card.numbers + Card.faces):
+        assert type(Game.rank2value[r]) in (int, float)
 
 
 def test_reshuffle_threshold():
