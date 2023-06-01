8f8411ce2e01aad27599437e9c119a6bc895ad92
jmmclaug201 <mclaughlinj201@gmail.com>
2023-06-01 14:36:56 -0400

    implement abstract Player class

---
+++ blackjack/player.py
@@ -0,0 +1,152 @@
+from abc import ABC, abstractmethod
+
+
+class Player(ABC):
+    @abstractmethod
+    def __init__(self, name="", bankroll=0):
+        """
+        Initializes player.
+
+        Params:
+            name (str): The player's name.
+                Req: None
+            bankroll (int): The player's starting bankroll.
+                Req: bankroll >= 0
+        """
+        pass
+
+    @abstractmethod
+    def sit_down(self, game_config):
+        """
+        Notifies the player that they are playing in a game.
+
+        Params:
+            game_config (GameConfig): The game_config of the game they will 
+                begin playing in.
+                Req: None
+        """
+        pass
+
+    @abstractmethod
+    def place_bet(self):
+        """
+        Places the player's bet for a round.
+
+        Returns (int):
+            bet -> The amount the player is betting.
+            Inv: bet >= 0
+
+        Side Effects:
+            Subtracts the bet amount from the player's bankroll.
+        """
+        pass
+
+    @abstractmethod
+    def observe_card(self, card, player):
+        """
+        Notifies the player of a card being assigned to some player.
+
+        Params:
+            card (Card): The card to be observed.
+                Req: None
+            player (int): The seat number of the player who received the card.
+                Req: player >= 0
+        """
+        pass
+
+    @abstractmethod
+    def start_hand(self, hand):
+        """
+        Gives the player a hand to play with.
+
+        Params:
+            hand (Hand): The hand the player will play with.
+            Req: None
+        """
+        pass
+
+    @abstractmethod
+    def decide_split(self):
+        """
+        Determines whether the player wishes to split their hand.
+
+        Returns (bool):
+            -> True if the player wishes to split their current hand
+            -> False otherwise
+
+        Side Effects:
+            -> Subtracts the additional bet from the bankroll if the player
+                decides to split.
+        """
+        pass
+
+    @abstractmethod
+    def decide_surrender(self):
+        """
+        Determine whether the player wishes to surrender their hand.
+
+        Returns (bool):
+            -> True if the player wishes to surrender their current hand
+            -> False otherwise
+        """
+        pass
+
+    @abstractmethod
+    def decide_double(self):
+        """
+        Determine whether the player wishes to double their hand.
+
+        Returns (bool):
+            -> True if the player wishes to double their current hand
+            -> False otherwise
+
+        Side Effects:
+            -> Subtracts the additional bet from the bankroll if the player
+                decides to double.
+        """
+        pass
+
+    @abstractmethod
+    def decide_hit(self):
+        """
+        Determine whether the player wishes to hit their hand.
+
+        Returns (bool):
+            -> True if the player wishes to hit their current hand
+            -> False otherwise
+        """
+        pass
+
+    @abstractmethod
+    def payout(self, payout):
+        """
+        Gives the player a payout for their current hand.
+
+        Params:
+            payout (int): The amount to pay out to the player.
+                Req: payout >= 0
+
+        Side Effects:
+            -> Adds the payout to the player's bankroll.
+        """
+        pass
+
+    def __str__(self):
+        """
+        Short string representation of the player.
+
+        Returns (str):
+            -> 'N' where N is the player's name
+        """
+        return self.name
+
+    def __repr__(self):
+        """
+        Long string representation of the player.
+
+        Returns (str):
+            'Player(N, B)' where 
+                -> N is the player's name
+                -> B is the player's bankroll
+        """
+        return f'Player({self.name}, {self.bankroll})'
