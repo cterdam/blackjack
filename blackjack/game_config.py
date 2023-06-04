ef71da73f86c0cc4edeca86a7e0bee1a1c7923d5
Abhishek <abhisheksingh.dev71@gmail.com>
2023-06-03 22:45:42 -0400

    Update GameConfig with new parameters

--- blackjack/game_config.py
+++ blackjack/game_config.py
@@ -1,8 +1,20 @@
-class GameConfig():
-
-    def __init__(self, num_decks=8, reshuffle_threshold=0.25,
-                 double_after_split=True, max_hands=4, normal_pay=1,
-                 blackjack_pay=3/2, natural_blackjack_only=True):
+class GameConfig:
+    def __init__(
+        self,
+        num_decks=8,
+        reshuffle_threshold=0.25,
+        double_after_split=True,
+        max_hands=4,
+        normal_pay=1,
+        blackjack_pay=3 / 2,
+        natural_blackjack_only=True,
+        min_bet=0,
+        max_bet=float("inf"),
+        int_bet_only=True,
+        blackjack_value=21,
+        max_turns=1,
+        init_hand_size=2,
+    ):
         """
         Config for a Blackjack game.
 
@@ -54,6 +66,30 @@ class GameConfig():
                 blackjack pay for any winning hand that contains as many cards
                 as the initial hand and sums to the blackjack value.
                 Req: None
+
+            min_bet (int or float): To begin a Blackjack hand, players must bet
+                at least the table minimum. For example, at a $5 to $500 table,
+                the minimum bet to play is $5.
+                Req: 0 <= min_bet
+
+            max_bet (int or float): The maximum amount that a player can bet in
+                a given hand.
+                Req: 0 <= max_bet
+
+            int_bet_only (boolean): If true, the player is allowed to place
+                only integer-valued bets.
+                Req: None
+
+            blackjack_value (int): Sum at which a hand triggers the blackjack
+                pay if dealt naturally.
+                Req: blackjack_value > 0
+
+            max_turns (int): The maximum number of permissable turns
+                Req: max_turns > 0
+
+            init_hand_size (int): The Number of cards each player starts with
+                in a hand
+                Req: init_hand_size >= 0
         """
 
         """ Init param check """
@@ -74,6 +110,23 @@ class GameConfig():
         assert type(blackjack_pay) in (int, float)
         # natural_blackjack_only should be int or float
         assert type(natural_blackjack_only) is bool
+        # min_bet should be non-negative int or float
+        assert type(min_bet) in (int, float)
+        assert min_bet >= 0
+        # max_bet should be non-negative int or float
+        assert type(max_bet) in (int, float)
+        assert max_bet >= 0
+        # int_bet_only should be bool
+        assert type(int_bet_only) is bool
+        # blackjack_value should be positive int
+        assert type(blackjack_value) is int
+        assert blackjack_value > 0
+        # max_turns should be positive int
+        assert type(max_turns) is int
+        assert max_turns > 0
+        # init_hand_size should be non-negative int
+        assert type(init_hand_size) is int
+        assert init_hand_size >= 0
 
         self.num_decks = num_decks
         self.reshuffle_threshold = reshuffle_threshold
@@ -82,3 +135,9 @@ class GameConfig():
         self.normal_pay = normal_pay
         self.blackjack_pay = blackjack_pay
         self.natural_blackjack_only = natural_blackjack_only
+        self.min_bet = min_bet
+        self.max_bet = max_bet
+        self.int_bet_only = int_bet_only
+        self.blackjack_value = blackjack_value
+        self.max_turns = max_turns
+        self.init_hand_size = init_hand_size
